53cae05ce7a696830202c85fbd7b2b09a64ee1f7
jinwenjie123 <jinwenjie00@gmail.com>
2022-08-31 11:59:18 -0700

    second commit

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors'; 
+
+import postRouters from './routes/posts.js';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+app.use('/posts', postRouters);
+
+//connect mongodb to our server
+const CONNECTION_URL = 'mongodb+srv://user1:Jinwenjie7@cluster0.8lwur4r.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+//mongoose.set('useFindAndModify', false);
+
